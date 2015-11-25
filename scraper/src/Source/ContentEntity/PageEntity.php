9caebb34f3fbad6c9f8cfe910c008315a4defec4
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2015-11-25 16:58:39 +0000

    Add content property to PageEntity

--- scraper/src/Source/ContentEntity/PageEntity.php
+++ scraper/src/Source/ContentEntity/PageEntity.php
@@ -3,11 +3,18 @@
 namespace Scraper\Source\ContentEntity;
 
 use Scraper\Source\Resource;
-use Scraper\Utility\ObjectCache;
+use Scraper\Utility\LazyProperties;
+use Scraper\Utility\TextHelper;
+use Symfony\Component\DomCrawler\Crawler;
 
 class PageEntity
 {
-    use ObjectCache;
+    use LazyProperties;
+
+    private $lazyProperties = [
+        'title',
+        'content',
+    ];
 
     public $resource = null;
 
@@ -19,48 +26,34 @@ class PageEntity
     public function getTitle()
     {
         $crawler = $this->resource->getCrawler();
-        $h1 = $crawler->filter('h1');
-        $title = $h1->text();
-        $title = utf8_decode($title);
-        $title = trim($title);
+        $title = $crawler->filter('h1')->text();
+        $title = TextHelper::tidyText($title);
         return $title;
     }
 
     public function getContent()
     {
         $crawler = $this->resource->getCrawler();
+        $filter = $crawler->filter('#content');
 
-        $elContent = $crawler->filter('#content');
-
-        eval(\Psy\sh());
-    }
-
-    /**
-     * Getter to provide content properties.
-     * Properties are handled by associated getProperty methods, and the output is cached to speed up subsequent calls.
-     *
-     * @param $property
-     * @return mixed
-     */
-    public function __get($property)
-    {
-        $contentProperties = [
-            'title',
-            'content',
-        ];
-
-        $getMethod = 'get' . ucfirst($property);
-
-        if (method_exists($this, $getMethod) && in_array($property, $contentProperties)) {
-            return $this->getObject($property, array($this, $getMethod));
+        // Clone elements into new Crawler instance
+        // This allows us to modify the DOM without modifying the DOM of the original crawler object.
+        $content = new Crawler();
+        foreach ($filter as $matchedElement) {
+            $content->add(clone $matchedElement);
         }
 
-        $trace = debug_backtrace();
-        trigger_error(
-            'Undefined or inaccessible property via __get(): ' . $property .
-            ' in ' . $trace[0]['file'] .
-            ' on line ' . $trace[0]['line'],
-            E_USER_NOTICE);
-        return null;
+        // Remove unwanted elements from content
+        $content->filter('h1')->each(function($crawler) {
+            foreach ($crawler as $node) {
+                $node->parentNode->removeChild($node);
+            }
+        });
+
+        // Filter and clean HTML
+        $html = $content->html();
+        $html = TextHelper::tidyHtml($html);
+
+        return $html;
     }
 }
