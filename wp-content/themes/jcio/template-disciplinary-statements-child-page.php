8b1c51f4ad392cabb5391d48bc97fd59d30a04e0
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2015-12-07 14:54:14 +0000

    List disciplinary statements on the page.

--- wp-content/themes/jcio/template-disciplinary-statements-child-page.php
+++ wp-content/themes/jcio/template-disciplinary-statements-child-page.php
@@ -6,5 +6,6 @@
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/page', 'header'); ?>
   <?php get_template_part('templates/tabs', 'disciplinary-statements'); ?>
+  <h2><?php the_title(); ?></h2>
   <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
--- wp-content/themes/jcio/template-disciplinary-statements.php
+++ wp-content/themes/jcio/template-disciplinary-statements.php
@@ -2,9 +2,41 @@
 /**
  * Template Name: Disciplinary Statements
  */
+
+$year = get_query_var('disciplinary_stmnt_year');
+
+$statements = new WP_Query(array(
+  'post_type' => 'disciplinary_stmnt',
+  'year' => $year,
+  'posts_per_page' => -1,
+));
+
 ?>
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/page', 'header'); ?>
   <?php get_template_part('templates/tabs', 'disciplinary-statements'); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
+  <h2><?php echo wptexturize($year); ?></h2>
+
+  <?php if ($statements->have_posts()): ?>
+    <ul>
+      <?php while ($statements->have_posts()): $statements->the_post(); ?>
+        <li>
+          <?php
+
+          $document = get_field('document');
+          $path = get_attached_file($document['ID']);
+          $extension = pathinfo($path, PATHINFO_EXTENSION);
+          $size = filesize($path);
+
+          ?>
+          <a href="<?php echo esc_url($document['url']); ?>" target="_blank" onclick="pageTracker._trackPageview('<?php echo esc_url($document['url']); ?>');">
+            <?php the_title(); ?>
+          </a>
+          [<?php echo strtoupper($extension); ?>, <?php echo size_format($size); ?>]
+        </li>
+      <?php endwhile; ?>
+    </ul>
+    <?php wp_reset_postdata(); ?>
+  <?php endif; ?>
+
 <?php endwhile; ?>
