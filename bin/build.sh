7ccddd27814efe624c3020d4766c04dc4119414e
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2019-05-03 19:01:53 +0100

    Fix vulnerabilities

--- bin/build.sh
+++ bin/build.sh
@@ -11,14 +11,14 @@ set -e
 composer install
 
 # build the .env file if it doesn't exist
-WP_ENV=.env
-if [[ ! -f "$WP_ENV" ]]; then
-    cp .env.example ${WP_ENV} && \
-    echo "$(tail -n +3 ${WP_ENV})" > ${WP_ENV} && \
-    WP_ENV_APP_NAME="$(basename `git rev-parse --show-toplevel`)" && \
-    WP_ENV_APP_NAME="$(echo ${WP_ENV_APP_NAME} | sed -e 's/wp-//g')" && \
-    sed -i "" "1s/^/SERVER_NAME=${WP_ENV_APP_NAME}.docker /" "${WP_ENV}"
-fi
+#WP_ENV=.env
+#if [[ ! -f "$WP_ENV" ]]; then
+#    cp .env.example ${WP_ENV} && \
+#    echo "$(tail -n +3 ${WP_ENV})" > ${WP_ENV} && \
+#    WP_ENV_APP_NAME="$(basename `git rev-parse --show-toplevel`)" && \
+#    WP_ENV_APP_NAME="$(echo ${WP_ENV_APP_NAME} | sed -e 's/wp-//g')" && \
+#    sed -i "" "1s/^/SERVER_NAME=${WP_ENV_APP_NAME}.docker /" "${WP_ENV}"
+#fi
 
 # Build theme assets here
 cd web/app/themes/jcio
