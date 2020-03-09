48f5cf20adab6e12d6f617c8511632c5637f892f
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2020-03-09 15:37:38 +0000

    Fixed serious syntax issue

--- web/app/themes/jcio/base.php
+++ web/app/themes/jcio/base.php
@@ -6,22 +6,25 @@ use Roots\Sage\Wrapper;
 ?>
 <!doctype html>
 <html class="no-js" <?php language_attributes(); ?>>
-  <body <?php body_class(); ?>>
-    <?php
-      do_action('get_header');
-      get_header();
-    ?>
-    <div id="contentwrapper">
-      <?php get_template_part('templates/main-menu'); ?>
-      <a id="skipnav" name="skipnav"></a>
-      <div id="content">
+<head>
+    <?php get_template_part('head'); ?>
+</head>
+<body <?php body_class(); ?>>
+<?php
+do_action('get_header');
+get_header();
+?>
+<div id="contentwrapper">
+    <?php get_template_part('templates/main-menu'); ?>
+    <a id="skipnav" name="skipnav"></a>
+    <div id="content">
         <?php include Wrapper\template_path(); ?>
-      </div>
     </div>
-    <?php
-      do_action('get_footer');
-      get_footer();
-      wp_footer();
-    ?>
-  </body>
+</div>
+<?php
+do_action('get_footer');
+get_footer();
+wp_footer();
+?>
+</body>
 </html>
--- web/app/themes/jcio/head.php
+++ web/app/themes/jcio/head.php
@@ -1,5 +1,5 @@
-<head>
+
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <?php wp_head(); ?>
-</head>
+
